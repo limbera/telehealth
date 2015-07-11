a5d7fee7a528ae1c76560df6c8910471260c2774
limbera <limbera@gmail.com>
2015-07-11 21:17:23 +1000

    headers for PA

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from twilio.rest import TwilioRestClient
 from twilio.rest.resources import Connection
 from twilio.rest.resources.connection import PROXY_TYPE_HTTP
 
-# proxy_url = os.environ.get("http_proxy")
-# host, port = urlparse(proxy_url).netloc.split(":")
-# Connection.set_proxy_info(host, int(port), proxy_type=PROXY_TYPE_HTTP)
+proxy_url = os.environ.get("http_proxy")
+host, port = urlparse(proxy_url).netloc.split(":")
+Connection.set_proxy_info(host, int(port), proxy_type=PROXY_TYPE_HTTP)
 
 app = Flask(__name__)
 port = int(os.getenv('VCAP_APP_PORT', 8080))
