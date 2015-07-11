0cab5e7c2198926752b80022fd81c6024b09954a
limbera <limbera@gmail.com>
2015-07-11 19:44:05 +1000

    twilio looping behaviour removed

--- app.py
+++ app.py
@@ -28,17 +28,50 @@ def hello_world():
     return 'Hello World! I am running on port ' + str(port)
 
 
-@app.route('/makeCall')
+@app.route('/makeCall', methods=['GET', 'POST'])
 def make_call():
     print 'Making a phone call to +61419190104'
     client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     call = client.calls.create(
-        url="http://coffeenomad.com.au/conversation.xml",
         to="+61419190104",
-        from_="+61281884735"
+        from_="+61281884735",
+        url="http://coffeenomad.com.au/conversation.xml"
     )
     return 'Making a phone call to +61419190104'
 
+@app.route('/handleRecording', methods=['GET', 'POST'])
+def handle_recording():
+    return
+    # recording_url = request.values.get('RecordingUrl', None)
+
+    # connection = httplib.HTTPSConnection('api.parse.com', 443)
+    # connection.connect()
+
+    # # upload the file to Parse.com /files/ dir 
+    # data = urllib2.urlopen(recording_url).read();
+    # connection.request('POST', '/1/files/audio.wav',  data, {
+    #    "X-Parse-Application-Id": PARSE_APP_ID,
+    #    "X-Parse-REST-API-Key": PARSE_REST_API_KEY,
+    #    'Content-Type': "audio/x-wav"
+    # })
+    # result = json.loads(connection.getresponse().read())
+
+    # connection = httplib.HTTPSConnection('api.parse.com', 443)
+    # connection.connect()
+    # connection.request('POST', '/1/classes/audio_recording', json.dumps({
+    #        "transcription": "Testing",
+    #        "audio_file": {
+    #          "name": result['name'],
+    #          "__type": "File"
+    #        }
+    #      }), {
+    #        "X-Parse-Application-Id": PARSE_APP_ID,
+    #        "X-Parse-REST-API-Key": PARSE_REST_API_KEY,
+    #        "Content-Type": "application/json"
+    #      })
+    # result = json.loads(connection.getresponse().read())
+    # print result
+    # return 'File successfully uploaded' + str(result)
 
 @app.route('/handleTranscription', methods=['GET', 'POST'])
 def transcribe_audio():
